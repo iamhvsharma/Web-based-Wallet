c74a9e1a1657aec494e6195258d77272d5a67a30
iamhvsharma <itshvsharma@gmail.com>
2024-10-14 14:22:46 +0530

    v1 Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 const Header = () => {
   return (
-    <div className='nav'>
-        <div className="logo">
-            <h1>Sandook</h1>
+    <div className='nav flex items-center justify-between px-20 py-6'>
+        <div className="logo flex">
+            <h1 className='text-lg font-semibold'>Sandook</h1>
+            <span className="rounded-full text-base bg-primary/10 border border-primary/50 px-2 ml-2">
+              v1
+            </span>
         </div>
+        <Button className=' bg-black text-white rounded-xl hover:bg-zinc-800 px-2 '>Dark Mode</Button>
       
     </div>
   )
