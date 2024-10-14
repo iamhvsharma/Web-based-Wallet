76f0252a9298ceb93b6182eef097d819f7d6ca82
iamhvsharma <itshvsharma@gmail.com>
2024-10-14 23:23:08 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from './ui/button'
 
 const Header = () => {
   return (
-    <div className='nav flex items-center justify-between px-20 py-6'>
+    <div className='nav flex items-center justify-between px-20 py-6 shadow-md'>
         <div className="logo flex">
             <h1 className='text-lg font-semibold'>Sandook</h1>
             <span className="rounded-full text-base bg-primary/10 border border-primary/50 px-2 ml-2">
