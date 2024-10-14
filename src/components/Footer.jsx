ecf9b056785ae3202095e789af4fe0a240a66976
iamhvsharma <itshvsharma@gmail.com>
2024-10-14 23:22:31 +0530

    Footer without Link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='w-full h-16 absolute bottom-0 flex items-center border-t'>
+      <div className='w-full'>
+      <h2 className='text-sm font-small px-20'>Designed and Developed by <span className='font-semibold'>Harshvardhan</span></h2>
+      </div>
+        
     </div>
   )
 }
